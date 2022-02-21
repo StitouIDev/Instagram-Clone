b2c3c3423eb7ed8718cfbfcbb38b492b25a238bb
Hamza <yourgitemail@example.com>
2022-02-21 20:06:26 +0100

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Hamza on 2/21/22.
+//  Copyright © 2022 Hamza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
